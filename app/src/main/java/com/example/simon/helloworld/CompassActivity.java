f2c5189b5ca8cda7f33b741ad41e94376751c853
Simon <tpi13sjo@student.lu.se>
2018-04-18 01:05:06 +0200

    Fourtth

--- app/src/main/java/com/example/simon/helloworld/CompassActivity.java
+++ app/src/main/java/com/example/simon/helloworld/CompassActivity.java
@@ -1,54 +1,134 @@
 package com.example.simon.helloworld;
 
+import android.content.DialogInterface;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.os.Vibrator;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.view.ContextThemeWrapper;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class CompassActivity extends AppCompatActivity implements SensorEventListener {
-    private SensorManager sensorManager;
-    private Sensor rotationVector;
-    private ImageView compassImg;
-    private TextView compassText;
-    private  float[] vector = new float[9];
-    private float[] orientation = new float[3];
-    private float azimuth;
+
+    ImageView compass_img;
+    TextView txt_compass;
+    int mAzimuth;
+    private SensorManager mSensorManager;
+    private Sensor mRotationV, mAccelerometer, mMagnetometer;
+    boolean haveSensor = false, haveSensor2 = false;
+    float[] rMat = new float[9];
+    float[] orientation = new float[3];
+    private float[] mLastAccelerometer = new float[3];
+    private float[] mLastMagnetometer = new float[3];
+    private boolean mLastAccelerometerSet = false;
+    private boolean mLastMagnetometerSet = false;
+
+    private Vibrator vibrator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compass);
 
-        sensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
-        compassImg = (ImageView) findViewById(R.id.compass);
-        compassText = (TextView) findViewById(R.id.textViewCompass);
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        compass_img = (ImageView) findViewById(R.id.compass);
+        txt_compass = (TextView) findViewById(R.id.textViewCompass);
 
-        rotationVector = sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
+        vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
+
+        start();
     }
 
-    protected void onResume() {
-        super.onResume();
-        sensorManager.registerListener(this, rotationVector, sensorManager.SENSOR_DELAY_NORMAL);
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_ROTATION_VECTOR) {
+            SensorManager.getRotationMatrixFromVector(rMat, event.values);
+            mAzimuth = (int) (Math.toDegrees(SensorManager.getOrientation(rMat, orientation)[0]) + 360) % 360;
+        }
+
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            System.arraycopy(event.values, 0, mLastAccelerometer, 0, event.values.length);
+            mLastAccelerometerSet = true;
+        } else if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
+            System.arraycopy(event.values, 0, mLastMagnetometer, 0, event.values.length);
+            mLastMagnetometerSet = true;
+        }
+        if (mLastAccelerometerSet && mLastMagnetometerSet) {
+            SensorManager.getRotationMatrix(rMat, null, mLastAccelerometer, mLastMagnetometer);
+            SensorManager.getOrientation(rMat, orientation);
+            mAzimuth = (int) (Math.toDegrees(SensorManager.getOrientation(rMat, orientation)[0]) + 360) % 360;
+        }
+
+        int pointer = Math.round(mAzimuth);
+        compass_img.setRotation(-pointer);
+
+        txt_compass.setText("Heading: " + mAzimuth + " degrees");
+
+        if(mAzimuth <= 15 || mAzimuth >= 345) {
+            vibrator.vibrate(500);
+        }
     }
 
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
+    public void start() {
+        if (mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR) == null) {
+            if ((mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) == null) || (mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD) == null)) {
+                noSensorsAlert();
+            }
+            else {
+                mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+                mMagnetometer = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+                haveSensor = mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_UI);
+                haveSensor2 = mSensorManager.registerListener(this, mMagnetometer, SensorManager.SENSOR_DELAY_UI);
+            }
+        }
+        else{
+            mRotationV = mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
+            haveSensor = mSensorManager.registerListener(this, mRotationV, SensorManager.SENSOR_DELAY_UI);
+        }
+    }
+
+    public void noSensorsAlert(){
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+        alertDialog.setMessage("Your device doesn't support the Compass.")
+                .setCancelable(false)
+                .setNegativeButton("Close",new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        finish();
+                    }
+                });
+        alertDialog.show();
+    }
+
+    public void stop() {
+        if (haveSensor) {
+            mSensorManager.unregisterListener(this, mRotationV);
+        }
+        else {
+            mSensorManager.unregisterListener(this, mAccelerometer);
+            mSensorManager.unregisterListener(this, mMagnetometer);
+        }
+    }
+
+    @Override
     protected void onPause() {
         super.onPause();
-        sensorManager.unregisterListener(this);
+        stop();
     }
 
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    @Override
+    protected void onResume() {
+        super.onResume();
+        start();
     }
 
-    public void onSensorChanged(SensorEvent event) {
-        SensorManager.getRotationMatrixFromVector(vector , event.values);
-        azimuth = (float) (Math.toDegrees(SensorManager.getOrientation(vector, orientation)[0]) + 180) % 360;
-
-        compassImg.setRotation(-azimuth);
-
-        compassText.setText("Heading: " + azimuth + "degrees");
-    }
 }
